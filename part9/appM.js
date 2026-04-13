cfa9fdb290ce32320a42d39b7aaed963a939a776
khushisharma2004 <ks969250@gmail.com>
2026-04-13 16:56:02 +0530

    mathopertaor

---
+++ part9/appM.js
@@ -0,0 +1,8 @@
+//Module import file
+//default  import
+import multiply from "./mathOperationM.js";
+//named import
+import {add,subtract} from "./mathOperationM.js";
+console.log(multiply(2,2));
+console.log(add(2,2));
+
