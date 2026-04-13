d3748dca41cce74f92b48de121ecca0af5841f98
khushisharma2004 <ks969250@gmail.com>
2026-04-13 16:16:17 +0530

    thisContext

---
+++ part9/thisContext.js
@@ -0,0 +1,13 @@
+const Person={
+    name:"Hitesh",
+    greet(){
+        console.log(`Hi,I am ${this.name}`);
+        
+    }, 
+};
+person.greet();
+const greetFunction =person.greet
+greetFunction();
+const boundGreet=person.greet.bind({name:"John"});
+boundGreet();
+//bind call and apply
